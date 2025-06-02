58a777a988d1f38794802e74826959001bcb77fc
BambiVisconti <cesar.visconti82@gmail.com>
2025-06-02 13:00:56 -0700

    gucci night

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import *
 import math
 import time
 
-casedepart=3
+casedepart=0
 nbcases = 12
 maisondessus = 0
 a=1
@@ -16,10 +16,13 @@ def permutation(i):
     lst=[1,2,4,5,7,8,10,11]
     return lst[i]
 
+
+
+
 def creerjoueurs(n):
     joueurs = {}
     for i in range(0,n):
-        joueurs[f'joueur{i}'] = {'indice': i, 'case': 0, 'argent': 200, 'propriete': [], 'validite': True, 'prison':0}
+        joueurs[f'joueur{i}'] = {'indice': i, 'case': 0, 'argent': 300, 'propriete': [], 'validite': True, 'prison':0}
     return joueurs
 
 def creerimmobilier(n):
@@ -28,11 +31,26 @@ def creerimmobilier(n):
        immobilier[f'appart{i}'] = {'indice': i, 'dispo': True, 'prix': 30+10*i, 'loyer': 10+2*i, 'case':permutation(i) , 'proprietaire':-1}
     return immobilier
     
+def perte(i,joueurs,immobilier):
+    joueurs[f'joueur{i}']['validite']=False
+    joueurs[f'joueur{i}']['argent']=0
+    for j in joueurs[f'joueur{i}']['propriete']:
+        immobilier[f'appart{j}']['dispo']=True
+    joueurs[f'joueur{i}']['propriete']=[]
+    print()
+    print("LE JOUEUR ",i," EST ELIMINE")
+    print()
 
-def chance(i):
+def chance(i,joueurs,immobilier):
     ent=randint(1,4)
-    if ent==1: joueurs[f'joueur{i}']['argent'] -= 10
-    if ent==2: joueurs[f'joueur{i}']['argent'] += 10
+    print(ent)
+    if ent==1:
+        if joueurs[f'joueur{i}']['argent'] > 5:
+            joueurs[f'joueur{i}']['argent'] -= 5
+        else:
+            perte(i,joueurs,immobilier)
+    
+    if ent==2: joueurs[f'joueur{i}']['argent'] += 5
     if ent==3:
         joueurs[f'joueur{i}']['case'] = 4
         joueurs[f'joueur{i}']['prison']= 3
@@ -40,16 +58,17 @@ def chance(i):
         joueurs[f'joueur{i}']['case'] = 0
         joueurs[f'joueur{i}']['argent'] += casedepart
 
-def simulation(nbjoueurs,nbtours,nbappart):
+
+def simulation(nbjoueurs,nbtours):
     joueurs = creerjoueurs(nbjoueurs)
-    immobilier = creerimmobilier(nbappart)
+    immobilier = creerimmobilier(8)
     nbrestant = nbjoueurs
     p=0
     while nbrestant != 1:
-        time.sleep(0.02)
         i=p%nbjoueurs
         p+=1
-        if joueurs[f'joueur{i}']['validite'] == False : a=1
+        if joueurs[f'joueur{i}']['validite'] == False:
+            joueurs[f'joueur{i}']['argent']=0
         elif joueurs[f'joueur{i}']['prison'] != 0: joueurs[f'joueur{i}']['prison'] -= 1
         else:
             move = de()
@@ -64,34 +83,38 @@ def simulation(nbjoueurs,nbtours,nbappart):
                         break
                 if maisondessus['dispo'] == True:
                     if joueurs[f'joueur{i}']['argent'] > maisondessus['prix']:
-                        joueurs[f'joueur{i}']['propriete'].append(maisondessus['case'])
+                        joueurs[f'joueur{i}']['propriete'].append(maisondessus['indice'])
                         joueurs[f'joueur{i}']['argent'] -= maisondessus['prix']
                         immobilier[f'appart{indiceappart}']['dispo'] = False
                         immobilier[f'appart{indiceappart}']['proprietaire'] = i
+                        print("le joueur ",i," achete le bien d'exception ",immobilier[f'appart{indiceappart}']['indice'])
                 else:
                     if joueurs[f'joueur{i}']['argent'] > maisondessus['loyer']:
                         joueurproprio = maisondessus['proprietaire']
+                        print("le joueur ",i," paye ",maisondessus['loyer']," de loyer au joueur ",maisondessus['proprietaire'])
                         joueurs[f'joueur{i}']['argent'] -= maisondessus['loyer']
                         joueurs[f'joueur{joueurproprio}']['argent'] += maisondessus['loyer']
                     else:
-                        joueurs[f'joueur{i}']['validite'] = False
+                        perte(i,joueurs,immobilier)
                         nbrestant -= 1
             else:
-                if joueurs[f'joueur{i}']['case']==0: joueurs[f'joueur{i}']['argent']+=2
+                if joueurs[f'joueur{i}']['case']==0: joueurs[f'joueur{i}']['argent']+=casedepart
                 if joueurs[f'joueur{i}']['case']==3: print("le joueur ",i," visite la prison")
                 if joueurs[f'joueur{i}']['case']==6:
+                    print("le joueur ",i," part en zonz")
                     joueurs[f'joueur{i}']['prison']=2
                     joueurs[f'joueur{i}']['case']=2
                 if joueurs[f'joueur{i}']['case']==9:
-                    #chance(i)
+                    chance(i,joueurs,immobilier)
                     print("carte chance!")
 
                     
 
-        print("Le joueur", i, " qui est en ",joueurs[f'joueur{i}']['case'],"a", joueurs[f'joueur{i}']['argent'],"e et a les appart ",joueurs[f'joueur{i}']['propriete'], joueurs[f'joueur{i}']['validite'])
+       #print("Le joueur", i, " qui est en ",joueurs[f'joueur{i}']['case'],"a", joueurs[f'joueur{i}']['argent'],"e et a les appart ",joueurs[f'joueur{i}']['propriete'], joueurs[f'joueur{i}']['validite'])
     for k in joueurs:
-        if joueurs[k]['validite']==True: return ("caca",joueurs[k]['indice'])
-    
+        if joueurs[k]['validite']==True:
+            print("Le vainqueur est ", joueurs[k]['indice']," avec un patrimoine de ",joueurs[k]['propriete'], "et une fortune de ",joueurs[k]['argent'],"e")
+            print("Cette partie a duré ", math.trunc(p/nbjoueurs)+1," tours")
 
                 
                         
