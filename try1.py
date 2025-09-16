ed1feedfaf9466e1cb72be292c3f1c76be828861
César Visconti <cesou@MacBook-Air-de-Cesar-2.local>
2025-09-16 18:08:48 +0200

    2push

--- try1.py
+++ try1.py
@@ -1,7 +1,12 @@
 import random
 
 joueurs = {}
+tours=0
 
+hot=[0 for j in range(40)]
+owners=[-1 for j in range(40)]
+price=[30+5*j for j in range(40)]
+rent=[j for j in range(40)]
 
 def createplayers(n, money):
     for i in range(n):
@@ -14,19 +19,21 @@ def createplayers(n, money):
         }
 
 def game(n,money,time): #time=0 infini
+    global tours
     tours=0
     createplayers(n,money)
     if time==0:
         while True:
             play(tours%n)
-            tours +=1
             if tours%n==0 :
                 if gamevalidity(n)==False:
+                    endgame(n)
                     break
     else:
         for j in range(0,time):
             play(tours%n)
-            tours +=1
+
+        endgame(n)
             
 
 def roll(i):
@@ -46,12 +53,23 @@ def play(i):
         
         
 def position(i):
+    global tours
+    tours+=1
     if joueurs[f"joueur{i}"]["pos"] in [1,3,6,8,9,11,13,14,16,18,19,21,23,24,26,27,29,31,32,34,37,39]: onproprio(i)
     elif joueurs[f"joueur{i}"]["pos"] in [5,15,25,35]: ontrain(i)
     elif joueurs[f"joueur{i}"]["pos"] in [7,22,36]: onluck(i)
     elif joueurs[f"joueur{i}"]["pos"] == 30:
         joueurs[f"joueur{i}"]["pos"]=10
-        joueurs[f"joueur{i}"]["jailtile"]=3
+        hot[joueurs[f"joueur{i}"]["pos"]]+=1
+        joueurs[f"joueur{i}"]["jailtime"]=3
+    else:
+        tours-=1
+
+def playervalidity(i):
+    if joueurs[f"joueur{i}"]["money"] < 0:
+        joueurs[f"joueur{i}"]["validity"]=False
+        for k in range(40):
+            if owners[k]==i: owners[k]= -1
 
 
 def gamevalidity(n):
@@ -62,14 +80,39 @@ def gamevalidity(n):
     else: return True
 
 def onproprio(i):
-    print("joueur ",i," sur la case ",joueurs[f"joueur{i}"]["pos"])
+    hot[joueurs[f"joueur{i}"]["pos"]]+=1
+    place = joueurs[f"joueur{i}"]["pos"]
+    if owners[place]==i: pass
+    elif owners[place]==-1:
+        if joueurs[f"joueur{i}"]["money"]> 1.5*(price[place]):
+            owners[place]=i
+            joueurs[f"joueur{i}"]["money"] += -price[place]
+            #print("le joueur ",i," achete propriété ",place)
+    else:
+        owner=owners[place]
+        joueurs[f"joueur{i}"]["money"] += -rent[place]
+        joueurs[f"joueur{owner}"]["money"] += rent[place]
+        playervalidity(i)
+        #print("le joueur ",i," tombe chez le joueur ",owner," et lui reste $",joueurs[f"joueur{i}"]["money"])
+        
 
 def ontrain(i):
-    print("joueur ",i," sur la case ",joueurs[f"joueur{i}"]["pos"])
+    hot[joueurs[f"joueur{i}"]["pos"]]+=1
 
 def onluck(i):
-    print("joueur ",i," sur la case ",joueurs[f"joueur{i}"]["pos"])
+    hot[joueurs[f"joueur{i}"]["pos"]]+=1
 
 
-
+def endgame(n):
+    for j in range(40):
+        hot[j]=(hot[j])/tours
+    print("La partie a durée ",int(tours/n)," tours")
+    print("les cases chaudes", hot)
+    print(joueurs)
+    print(owners)
+    a=0
+    for j in hot: a+=j 
+    print(a)
+        
+    
     
